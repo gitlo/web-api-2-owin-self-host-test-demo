e3b2e32e7e1b633da774cb2646313aeb32850c7e
Kez Nwichi <Kez.Nwichi@chambersandpartners.co.uk>
2016-05-09 18:29:35 +0100

    project added

---
+++ WebApi2Demo/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace WebApi2Demo
+{
+    public class Startup
+    {
+        // This method is required by Katana:
+        public void Configuration(IAppBuilder app)
+        {
+            var webApiConfiguration = new HttpConfiguration();
+
+            WebApiConfig.Register(webApiConfiguration);
+            // Use the extension method provided by the WebApi.Owin library:
+            app.UseWebApi(webApiConfiguration);
+        }
+
+    }
+}
---
+++ e2eTests/TestServiceCall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EndersJson;
+using EndersJson.Interfaces;
+using Microsoft.Owin.Hosting;
+using NUnit.Framework;
+using WebApi2Demo;
+
+namespace e2eTests
+{
+    public class TestServiceCall
+    {
+        readonly IJsonService _client = new JsonService();
+
+        [Test]
+        public async Task TestFoo()
+        {
+            using (WebApp.Start<Startup>("http://localhost:9030"))
+            {
+                var result = await _client.Get<string>("http://localhost:9030/api/values/1");
+                Assert.That(result, Is.EqualTo("value"));
+            }
+        }
+    }
+}
